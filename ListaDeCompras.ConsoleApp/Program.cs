53a498c7ec8d8aac5ac64434a98eedc84f1ae49c
otaviobrignoni <otavio.678.b@gmail.com>
2026-04-30 14:12:08 -0300

    Adicionada estrutura do menu principal

--- ListaDeCompras.ConsoleApp/Program.cs
+++ ListaDeCompras.ConsoleApp/Program.cs
@@ -1,9 +1,28 @@
-﻿namespace ListaDeCompras.ConsoleApp;
+﻿using System.Text;
+using ListaDeCompras.ConsoleApp.Shared;
+
+namespace ListaDeCompras.ConsoleApp;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.OutputEncoding = Encoding.UTF8;
+
+        string title = Utils.ColourStringHex("Lista de Compras", Colours.Title);
+        string[] options = ["Categorias", "Produtos", "Lista de Compras", "Sair"];
+
+        while (true)
+            switch (Utils.Menu(title, options))
+            {
+                case 0:
+                    break;
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    return;
+            }
     }
 }
